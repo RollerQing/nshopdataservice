6c2edac58789ae718bf4d6178ddb98c1afddcdb4
RollerQing <1198319583@qq.com>
2019-09-28 14:43:04 +0800

    this is first commit

---
+++ src/main/java/com/nshop/nshopdataservice/NshopdataserviceApplication.java
@@ -0,0 +1,13 @@
+package com.nshop.nshopdataservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NshopdataserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NshopdataserviceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nshop/nshopdataservice/NshopdataserviceApplicationTests.java
@@ -0,0 +1,16 @@
+package com.nshop.nshopdataservice;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class NshopdataserviceApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
